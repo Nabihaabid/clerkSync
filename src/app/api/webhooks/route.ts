fc1bc1656857f42d50e05411a0ae9ae007c492c0
Nabiha Abid <nabihaabid1000@gmail.com>
2025-05-04 20:17:42 +0500

    next changes

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -10,7 +10,7 @@ export async function POST(req: NextRequest) {
     const { id } = evt.data;
     const eventType = evt.type;
 
-    console.log(Received webhook with ID ${id} and event type of ${eventType});
+    console.log(`Received webhook with ID ${id} and event type of ${eventType}`);
     // console.log("Webhook payload:", evt.data);
 
     if (evt.type === "user.created") {
